c22e54a4401e05f540d64e3486a97ad4f14beedf
Sasaki Yusuke <amagiri2501@yahoo.co.jp>
2017-11-25 00:32:09 +0900

    終了時のウィンドウサイズ、位置を保存する機能を追加

--- main.js
+++ main.js
@@ -4,22 +4,39 @@ var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
 
+const Config = require('electron-config');
+
+const config = new Config({
+	defaults: {
+		bounds: {
+			width: 800,
+			height: 600,
+		},
+	},
+})
+
 var mainWindow = null;
+var bounds_info;
 
 // 画面がすべて閉じたらアプリを終了する
 app.on('window-all-closed', function() {
-    if (process.platform != 'darwin')
-    app.quit();
+	if (process.platform != 'darwin')
+	app.quit();
 });
 
 app.on('ready', function() {
 
-    // ブラウザ(Chromium)の起動, 400 * 300
-    mainWindow = new BrowserWindow({width:300, height: 400});
-    // 最初に読み込む画面のパスを指定
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+	const {width, height, x, y} = config.get('bounds');
+	// ブラウザ(Chromium)の起動, 400 * 300
+	mainWindow = new BrowserWindow({width, height, x, y});
+	// 最初に読み込む画面のパスを指定
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
+	mainWindow.on('closed', function() {
+		mainWindow = null;
+	});
+
+	mainWindow.on('close', function() {
+		config.set('bounds', mainWindow.getBounds());
+	});
 });
